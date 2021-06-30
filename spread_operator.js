c3ac579a41ffbbae30686cb0f767c26fd03a0bf3
whydandrian <whydandrian@gmail.com>
2021-06-30 22:36:58 +0700

    implement spread to show mhs data

---
+++ spread_operator.js
@@ -0,0 +1,31 @@
+// Spread Operator
+// Memecah iterables menjadi single element
+
+// const mhs = ['Wahyudi', 'Eko', 'David'];
+
+// console.log(...mhs[0]);
+
+// Dapat menggabungkan 2 array
+// const mhs = ['Wahyudi', 'Eko', 'David'];
+// const dosen = ['Dono', 'Andi', 'Dina'];
+//berikut spread operator
+// const orang = [...mhs,...dosen];
+
+// const orang = mhs.concat(dosen);
+
+// console.log(orang);
+// Meng Copy Array
+// const mhs = ['Wahyudi', 'Eko', 'David'];
+// const mhs1 = mhs;
+// const mhs1 = [...mhs];
+// mhs[1] = 'Bebunm';
+// console.log(mhs);
+
+const liMhs = document.querySelectorAll('li');
+// console.log(liMhs);
+// const mhs = [];
+// for (let i = 0; i < liMhs.length; i++) {
+//   mhs.push(liMhs[i].textContent)  
+// }
+// console.log(mhs);
+
