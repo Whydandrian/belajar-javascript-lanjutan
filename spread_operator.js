3d6e48be8d63721d7e9d19d75072cdd9ced3e891
whydandrian <whydandrian@gmail.com>
2021-07-01 21:06:02 +0700

    modify : spreading element h1 to add animation in css

--- spread_operator.js
+++ spread_operator.js
@@ -21,11 +21,14 @@
 // mhs[1] = 'Bebunm';
 // console.log(mhs);
 
-const liMhs = document.querySelectorAll('li');
+// const liMhs = document.querySelectorAll('li');
 // console.log(liMhs);
 // const mhs = [];
 // for (let i = 0; i < liMhs.length; i++) {
 //   mhs.push(liMhs[i].textContent)  
 // }
+// const mhs = [...liMhs].map(m => m.textContent);
 // console.log(mhs);
-
+const nama = document.querySelector('.nama');
+const huruf = [...nama.textContent].map(h => `<span>${h}</span>`).join('');
+nama.innerHTML = huruf;
