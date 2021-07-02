1b1281b800ef4c85a79954f8606d563cf77a44ef
whydandrian <whydandrian@gmail.com>
2021-07-02 22:45:58 +0700

    add array object to simulation synchronus using forEach

--- callback.js
+++ callback.js
@@ -1,4 +1,47 @@
-// Callbak
-function halo() {
-  
-}
+// Callback
+// Hal dibawah ini disebut Syncrhonus Callback
+// function halo(nama) {
+//   alert(`Halo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//   const nama = prompt('Masukkan nama anda : ');
+//   callback(nama);
+// }
+
+// tampilkanPesan(nama => alert(`Halo ${nama}`));
+
+// const mhs = [
+//   {
+//     "nama": "Wahyudi Andrian",
+//     "nrp": "20172010",
+//     "email": "whydandrian@gmail.com",
+//     "jurusan": "Manajemen Informatika",
+//     "idDosenWali": 1
+//   },
+//   {
+//     "nama": "Ahmad Jamaluddin",
+//     "nrp": "20170912",
+//     "email": "jamall@gmail.com",
+//     "jurusan": "Manajemen Informatika",
+//     "idDosenWali": 2
+//   },
+//   {
+//     "nama": "Sinta Purnamasari",
+//     "nrp": "20170102",
+//     "email": "sinta@gmail.com",
+//     "jurusan": "Manajemen Informatika",
+//     "idDosenWali": 2
+//   }
+// ];
+
+// console.log('Mulai');
+// mhs.forEach(m => {
+//   //Simulasi Synchronus
+//   for (let i = 0; i < 10000000; i++) {
+//     let date = new Date();    
+//   }
+//   console.log(m.nama)
+});
+console.log('selesai');
+
